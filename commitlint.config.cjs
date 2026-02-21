9070cb994bf931f38a0ef96cb1e0b4c5dffdc623
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2026-02-21 14:11:27 +0900

    docs(#1): 이슈/PR/커밋 워크플로우 템플릿 문서화
    - 이슈 템플릿에 배경/목표/기대효과/리스크 항목을 추가함\n- PR 템플릿에 표준 섹션과 셀프 체크리스트를 정의함\n- commitlint 설정과 협업 워크플로우 문서를 신규 작성함

---
+++ commitlint.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  plugins: [
+    {
+      rules: {
+        'scope-issue-number': ({ scope }) => {
+          const valid = typeof scope === 'string' && /^#\d+$/.test(scope);
+          return [
+            valid,
+            '커밋 스코프는 #이슈번호 형식이어야 합니다. 예: feat(#123): 메시지',
+          ];
+        },
+      },
+    },
+  ],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'scope-issue-number': [2, 'always'],
+    'body-empty': [2, 'never'],
+  },
+};
